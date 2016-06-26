5b9c9dffd138af08abbbf0c946cf36f97f11dc4d
Ian Hatton <ian@morsedigital.com>
2016-06-26 14:29:02 +0100

    Amended requie

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-var vanillaCarousel = require('./lib/vanilla-carousel');
-module.exports = vanillaCarousel;
+module.exports = require('./lib/vanilla-carousel');
+
+// Alternatively:
+
+// var vanillaCarousel = require('./lib/vanilla-carousel');
+// module.exports = vanillaCarousel;
 
 // let vanillaCarousel = require("@ianhatton/vanilla-carousel");
+
+// Or even:
 
 // module.exports = {
 //   blah: vanillaCarousel
